7042dee654d5865c827aa6891c7c0ba4ccd4d754
Carlos <carlosbarroso2506@gmail.com>
2024-12-11 12:21:04 +0100

    Actualizando repos

--- Horas Libre Configuracion/Tema1/Ejemplos/EjemploHerencia/EjemploHerencia/EjemploHerencia/Constantes.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-    public static class Constantes
-    {
-        public const int NUMLADOS = 3;
-        public const int MEDLADOS = 1;
-        public const int CUADRADO = 4;
-        public const int TRIANGULO = 3;
-    }
