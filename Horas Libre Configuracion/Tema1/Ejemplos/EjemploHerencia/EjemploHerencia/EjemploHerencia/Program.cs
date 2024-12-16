e5507105dc69789a5f6458d8062b72ca4cd6f8af
Carlos <carlosbarroso2506@gmail.com>
2024-12-16 12:12:23 +0100

    Actualizando repos

--- Horas Libre Configuracion/Tema1/Ejemplos/EjemploHerencia/EjemploHerencia/EjemploHerencia/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EjemploHerencia;
-
-Cuadrado cuadrado = new Cuadrado();
-
-Console.WriteLine(cuadrado.ToString());
-
-Triangulo triangulo = new Triangulo();
