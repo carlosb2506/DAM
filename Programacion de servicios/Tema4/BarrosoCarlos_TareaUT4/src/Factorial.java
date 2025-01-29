67d5e74c3025cb7037d3c4d84ebb528ef60a1bea
carlosPC <cbarlop2506@gmail.com>
2025-01-30 00:10:30 +0100

    Actualizando repos

--- Programacion de servicios/Tema4/BarrosoCarlos_TareaUT4/src/Factorial.java
+++ Programacion de servicios/Tema4/BarrosoCarlos_TareaUT4/src/Factorial.java
@@ -7,61 +7,54 @@ import java.math.BigInteger;
 
 public class Factorial implements HttpHandler {
 
-	private HttpServer server;
+	private HttpServer servidor;
 
-	// Constructor que recibe el servidor como un parámetro
-	public Factorial(HttpServer server) {
-		this.server = server;
+	public Factorial(HttpServer servidor) {
+		this.servidor = servidor;
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		String response;
+		String respuesta;
 
 		if ("GET".equals(exchange.getRequestMethod())) {
 
-			// Obtiene los parámetros de la URL de la solicitud.
 			String query = exchange.getRequestURI().getQuery();
 
 			// Verifica si hay un parámetro 'number' en la consulta.
 			if (query != null && query.startsWith("number=")) {
 				try {
-					// Extraemos el valor del parámetro 'number' y le hacemos casting a entero.
-					int number = Integer.parseInt(query.split("=")[1]);
+
+					int numero = Integer.parseInt(query.split("=")[1]);
 
 					// Verificamos que el numero no sea negativo.
-					if (number < 0) {
-						response = "EL NÚMERO NO PUEDE SER NEGATIVO";
+					if (numero < 0) {
+						respuesta = "EL NÚMERO NO PUEDE SER NEGATIVO";
 					} else {
 
 						// Calculamos el factorial del número.
-						BigInteger factorial = calculateFactorial(number);
-						response = "EL FACTORIAL DE: " + number + " ES: " + factorial;
+						BigInteger factorial = calculateFactorial(numero);
+						respuesta = "EL FACTORIAL DE: " + numero + " ES: " + factorial;
 					}
 				} catch (NumberFormatException e) {
 
-					response = "EL PARÁMETRO 'NUMBER' DEBE SER UN NÚMERO ENTERO";
+					respuesta = "EL PARÁMETRO 'NUMBER' DEBE SER UN NÚMERO ENTERO";
 				}
 			} else {
 
-				response = "POR FAVOR, PROPORCIONA UN NÚMERO USANDO EL PARAMETRO 'NUMBER'. EJEMPLO: /factorial?number=5";
+				respuesta = "POR FAVOR, PROPORCIONA UN NÚMERO USANDO EL PARAMETRO 'NUMBER'. EJEMPLO: /factorial?number=5";
 			}
 		} else {
 
-			response = "MÉTODO NO SOPORTADO";
+			respuesta = "MÉTODO NO SOPORTADO";
 		}
 
 		exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
 
-		// Envia el código de respuesta HTTP (200 OK) y el contenido de la respuesta.
-		exchange.sendResponseHeaders(200, response.getBytes().length);
+		exchange.sendResponseHeaders(200, respuesta.getBytes().length);
 		try (OutputStream os = exchange.getResponseBody()) {
-			os.write(response.getBytes());
+			os.write(respuesta.getBytes());
 		}
-
-		// Detiene el servidor después de manejar la solicitud
-//		server.stop(0);
-//		System.out.println("SERVIDOR DETENIDO.");
 	}
 
 	public BigInteger calculateFactorial(int n) {
